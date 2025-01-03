d47cf3bb11d69b829014f89b7f0b8ed589efb189
Вера <yagina.vera@gmail.com>
2025-01-03 08:21:28 +0300

    added info widget

--- src/app/pages/LoanPage/LoanPage.tsx
+++ src/app/pages/LoanPage/LoanPage.tsx
@@ -1,10 +1,12 @@
 import '@styles/config/index.scss'
+import { Info } from '@widgets/Info/Info'
 import { Suggestion } from '@widgets/Suggestion/Suggestion'
 
 export const LoanPage = () => {
   return (
     <main className="loan page">
       <Suggestion />
+      <Info/>
     </main>
   )
 }
